a732d65b454d369487371ecc88e548d7904a8374
rp-guruh <github.guruh@gmail.com>
2025-05-22 16:29:20 +0700

    batas wilayah

--- app/Filament/Resources/BatasWilayahResource/Widgets/BatasWilayahOverview.php
+++ app/Filament/Resources/BatasWilayahResource/Widgets/BatasWilayahOverview.php
@@ -11,42 +11,42 @@ class BatasWilayahOverview extends BaseWidget
     protected function getStats(): array
     {
         $batasWilayah = BatasWilayah::first();
-        return [
-            Stat::make('Batas Utara', $batasWilayah->batas_utara 
-                ? $batasWilayah->batas_utara 
-                : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
-                )
-                ->descriptionIcon('heroicon-m-map-pin')
-                ->description('Perbatasan utara desa jongjong')
-                ->color('success'),
-            Stat::make('Batas Timur', $batasWilayah->batas_timur 
-                ? $batasWilayah->batas_timur 
-                : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
-                )
-                ->descriptionIcon('heroicon-m-map-pin')
-                ->description('Perbatasan timur desa jongjong')
-                ->color('success'),
-            Stat::make('Batas Selatan', $batasWilayah->batas_selatan 
-                ? $batasWilayah->batas_selatan 
-                : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
-                )
-                ->descriptionIcon('heroicon-m-map-pin')
-                ->description('Perbatasan selatan desa jongjong')
-                ->color('success'),
-            Stat::make('Batas Barat', $batasWilayah->batas_barat 
-                ? $batasWilayah->batas_barat 
-                : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
-                )
-                ->descriptionIcon('heroicon-m-map-pin')
-                ->description('Perbatasan barat desa jongjong')
-                ->color('success'),
-            Stat::make('Luas Wilayah', $batasWilayah->luas_wilayah 
-                ? $batasWilayah->luas_wilayah 
-                : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
-                )
-                ->descriptionIcon('heroicon-m-map')
-                ->description('dalam km² (kilometer persegi)')
-                ->color('success'),
+            return [
+                Stat::make('Batas Utara', $batasWilayah?->batas_utara
+                    ? $batasWilayah->batas_utara
+                    : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
+                    )
+                    ->descriptionIcon('heroicon-m-map-pin')
+                    ->description('Perbatasan utara desa jongjong')
+                    ->color('success'),
+                Stat::make('Batas Barat', $batasWilayah?->batas_barat
+                    ? $batasWilayah->batas_barat
+                    : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
+                    )
+                    ->descriptionIcon('heroicon-m-map-pin')
+                    ->description('Perbatasan barat desa jongjong')
+                    ->color('success'),
+                Stat::make('Batas Selatan', $batasWilayah?->batas_selatan
+                    ? $batasWilayah->batas_selatan
+                    : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
+                    )
+                    ->descriptionIcon('heroicon-m-map-pin')
+                    ->description('Perbatasan selatan desa jongjong')
+                    ->color('success'),
+                Stat::make('Batas Timur', $batasWilayah?->batas_timur
+                    ? $batasWilayah->batas_timur
+                    : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
+                    )
+                    ->descriptionIcon('heroicon-m-map-pin')
+                    ->description('Perbatasan timur desa jongjong')
+                    ->color('success'),
+                Stat::make('Luas Wilayah', $batasWilayah?->luas_wilayah
+                    ? $batasWilayah->luas_wilayah
+                    : new HtmlString('<span style="font-style: italic; color: #6b7280;">Belum ada data</span>')
+                    )
+                    ->descriptionIcon('heroicon-m-map-pin')
+                    ->description('Luas wilayah desa jongjong dalam km² (kilometer persegi)')
+                    ->color('success'),
         ];
     }
 }
